bd4cb8463ea11168c7eda40c1c0d37f038dda354
dewanto.rentenservice@googlemail.com <dewanto.rentenservice@googlemail.com@920737b8-6d35-43a0-81df-39543d9ce713>
2012-05-24 13:31:43 +0000

    initial import

---
+++ java/components/extra-configplugin/src/main/java/de/extra/client/plugins/configPlugin/ConfigConstants.java
@@ -0,0 +1,15 @@
+package de.extra.client.plugins.configPlugin;
+
+public class ConfigConstants {
+
+	// Schema f�r das unmarshalling der Konfigurations-Files
+
+	public static final String UNMARSHAL_CONFIG = "de.drv.dsrv.schema";
+
+	// Informationen f�r die Zuweisung von Werten im ConfigFile
+
+	public static final String PROFIL_DATA = "Data";
+	public static final String PROFIL_PACKAGE = "xres:Package";
+	public static final String PROFIL_MESSAGE = "xres:Message";
+
+}
