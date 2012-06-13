23b824fef49eb444bd6b01fe8363eacfbd3395ad
dewanto.rentenservice@googlemail.com <dewanto.rentenservice@googlemail.com@920737b8-6d35-43a0-81df-39543d9ce713>
2012-06-13 13:43:02 +0000

    initial import extra-dataplugin

---
+++ trunk/java/components/extra-dataplugin/src/main/java/de/extra/client/plugins/dataplugin/controller/DataPluginController.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package de.extra.client.plugins.dataplugin.controller;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import de.extra.client.core.model.VersanddatenBean;
+import de.extra.client.plugins.dataplugin.auftragssatz.AuftragssatzType;
+import de.extra.client.plugins.dataplugin.helper.DataPluginHelper;
+import de.extra.client.plugins.dataplugin.interfaces.IDataPluginController;
+
+public class DataPluginController implements IDataPluginController {
+
+	private final DataPluginHelper dataPluginHelper;
+
+	public DataPluginController(final DataPluginHelper dataPluginHelper) {
+		super();
+		this.dataPluginHelper = dataPluginHelper;
+	}
+
+	/**
+	 * Verarbeitungs-Controller f�r das DataPlugin
+	 */
+	@Override
+	public List<VersanddatenBean> processData() {
+		List<VersanddatenBean> versanddatenBeanList = new ArrayList<VersanddatenBean>();
+
+		List<String> nutzfileList = new ArrayList<String>();
+
+		// Ermitteln der Nutzdaten
+		nutzfileList = dataPluginHelper.getNutzfiles();
+
+		// Befuellen der Versanddaten-Liste
+		for (Iterator<String> iter = nutzfileList.iterator(); iter.hasNext();) {
+			String filename = iter.next();
+
+			VersanddatenBean versanddatenBean = new VersanddatenBean();
+
+			versanddatenBean.setNutzdaten(dataPluginHelper
+					.getNutzdaten(filename));
+
+			AuftragssatzType auftragssatz = new AuftragssatzType();
+
+			String auftragssatzName = filename + ".auf";
+
+			auftragssatz = dataPluginHelper
+					.unmarshalAuftragssatz(auftragssatzName);
+
+			// Setzen der RequestId
+			versanddatenBean.setRequestId(auftragssatz.getRequestId());
+
+			versanddatenBean = dataPluginHelper.fuelleVersandatenBean(
+					versanddatenBean, auftragssatz);
+
+			versanddatenBeanList.add(versanddatenBean);
+		}
+
+		return versanddatenBeanList;
+	}
+}
---
+++ trunk/java/components/extra-dataplugin/src/main/java/de/extra/client/plugins/dataplugin/dummy/DataPlugin.java
@@ -0,0 +1,114 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package de.extra.client.plugins.dataplugin.dummy;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import de.extra.client.core.model.CompressionPluginBean;
+import de.extra.client.core.model.DataSourcePluginBean;
+import de.extra.client.core.model.EncryptionPluginBean;
+import de.extra.client.core.model.PlugindatenBean;
+import de.extra.client.core.model.VersanddatenBean;
+import de.extra.client.core.plugin.IDataPlugin;
+
+public class DataPlugin implements IDataPlugin {
+
+	@Override
+	public List<VersanddatenBean> getVersandDaten() {
+
+		List<VersanddatenBean> versanddatenList = new ArrayList<VersanddatenBean>();
+
+		versanddatenList.add(loadVersanddaten());
+
+		return versanddatenList;
+	}
+
+	/**
+	 * Dummy-Klasse mit statischen Werten
+	 * 
+	 * @return Versanddatenbean
+	 */
+	private VersanddatenBean loadVersanddaten() {
+
+		VersanddatenBean vb = new VersanddatenBean();
+		CompressionPluginBean compressionPlugin = new CompressionPluginBean();
+		EncryptionPluginBean encryptionPlugin = new EncryptionPluginBean();
+		DataSourcePluginBean dataSourcePlugin = new DataSourcePluginBean();
+
+		String nutzdaten = "Testdaten";
+
+		// Nutzdaten setzen
+
+		vb.setNutzdaten(nutzdaten.getBytes());
+
+		// Compression-Infos setzen
+
+		compressionPlugin.setOrder(1);
+		compressionPlugin
+				.setCompAlgoId("http://www.extra-standard.de/transforms/compression/NONE");
+		compressionPlugin.setCompAlgoVers("1.0");
+		compressionPlugin.setCompAlgoName("KKS");
+
+		compressionPlugin.setCompSpecUrl("http://www.datentausch.de");
+		compressionPlugin.setCompSpecName("KKS");
+		compressionPlugin.setCompSpecVers("1.0");
+
+		compressionPlugin.setCompInput(200);
+		compressionPlugin.setCompOutput(100);
+
+		// Encryption-Infos setzen
+
+		encryptionPlugin.setOrder(2);
+		encryptionPlugin
+				.setEncAlgoId("http://www.extra-standard.de/transforms/encryption/PKCS7");
+		encryptionPlugin.setEncAlgoVers("1.3");
+		encryptionPlugin.setEncAlgoName("KKS");
+
+		encryptionPlugin.setEncSpecUrl("http://www.datentausch.de");
+		encryptionPlugin.setEncSpecName("KKS");
+		encryptionPlugin.setEncSpecVers("1.5.1");
+
+		encryptionPlugin.setEncInput(100);
+		encryptionPlugin.setEncOutput(200);
+
+		// DataSource-Infos setzen
+
+		dataSourcePlugin.setDsType("http://extra-standard.de/container/FILE");
+		dataSourcePlugin.setDsName("EDUA0000003");
+
+		Date datum = new Date();
+		datum.setMonth(0);
+		datum.setDate(12);
+		datum.setYear(110);
+		dataSourcePlugin.setDsCreated(datum);
+		dataSourcePlugin.setDsEncoding("I8");
+
+		List<PlugindatenBean> pluginList = new ArrayList<PlugindatenBean>();
+
+		pluginList.add(compressionPlugin);
+		pluginList.add(encryptionPlugin);
+		pluginList.add(dataSourcePlugin);
+
+		vb.setPlugins(pluginList);
+
+		return vb;
+	}
+}
