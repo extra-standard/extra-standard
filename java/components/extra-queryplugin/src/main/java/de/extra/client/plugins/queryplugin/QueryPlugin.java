837692cf8984763d5c94597d06e9c0ff7ca203a6
dewanto.rentenservice@googlemail.com <dewanto.rentenservice@googlemail.com@920737b8-6d35-43a0-81df-39543d9ce713>
2012-06-14 09:45:31 +0000

    initial import extra-queryplugin

---
+++ java/components/extra-queryplugin/src/main/java/de/extra/client/plugins/queryplugin/QueryPlugin.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package de.extra.client.plugins.queryplugin;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import de.extra.client.core.ExtraClient;
+import de.extra.client.core.model.VersanddatenBean;
+import de.extra.client.core.plugin.IDataPlugin;
+import de.extra.client.plugins.queryplugin.interfaces.IQueryPluginController;
+
+public class QueryPlugin implements IDataPlugin {
+
+	/**
+	 * Pfad und Dateiname in der die SpringConfig.xml liegt
+	 */
+	private static String SPRING_XML_FILE_PATH = "QueryPlugin.xml";
+
+	/**
+	 * Dateipfad der log4jProperties
+	 */
+	private static final String LOG_4_J_FILE = "log4j.properties";
+
+	private static Logger logger = Logger.getLogger(ExtraClient.class);
+
+	@Override
+	public List<VersanddatenBean> getVersandDaten() {
+
+		PropertyConfigurator.configureAndWatch(LOG_4_J_FILE);
+		logger.info("Start des Versands");
+
+		// Spring Beans laden.
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				SPRING_XML_FILE_PATH);
+
+		// Laden des Controlers
+
+		IQueryPluginController controller = (IQueryPluginController) applicationContext
+				.getBean("queryController");
+
+		if (logger.isDebugEnabled()) {
+
+			logger.debug("Erstelle Query");
+
+		}
+
+		return controller.processQuery();
+	}
+}
---
+++ java/components/extra-queryplugin/src/main/java/de/extra/client/plugins/queryplugin/helper/QueryHelper.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package de.extra.client.plugins.queryplugin.helper;
+
+import java.math.BigInteger;
+
+import javax.management.Query;
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
+
+public class QueryHelper {
+
+	/**
+	 * 
+	 * Hilfsklasse zum Aufbau des DataRequests
+	 * 
+	 * @param startId
+	 *            StartID aus der Property-Datei
+	 * @param packageLimit
+	 *            Maximale Anzahl der Pakete aus der Property-Datei
+	 * @return DataRequest mit der Query
+	 */
+	public DataRequest createQuery(String startId, String packageLimit) {
+
+		DataRequest dataRequest = new DataRequest();
+
+		Control controlElement = new Control();
+		Query query = new Query();
+		DataRequestArgument dataRequestArgument = new DataRequestArgument();
+		Operand operand = new Operand();
+		operand.setValue(startId);
+
+		// Setzen des Tags
+
+		QName qname = new QName("xs:string");
+		JAXBElement<Operand> jaxbOperand = new JAXBElement<Operand>(new QName(
+				"http://www.extra-standard.de/namespace/message/1", "GE"),
+				Operand.class, operand);
+		jaxbOperand.setValue(operand);
+
+		// Setzen der Property
+
+		dataRequestArgument
+				.setProperty("http://www.extra-standard.de/property/ResponseID");
+		dataRequestArgument.setType(qname);
+		dataRequestArgument.getContent().add(jaxbOperand);
+
+		query.getArgument().add(dataRequestArgument);
+
+		// Bef�llen des Control-Arguments
+
+		controlElement.setMaximumPackages(new BigInteger(packageLimit));
+
+		dataRequest.setQuery(query);
+		dataRequest.setControl(controlElement);
+
+		return dataRequest;
+	}
+}
