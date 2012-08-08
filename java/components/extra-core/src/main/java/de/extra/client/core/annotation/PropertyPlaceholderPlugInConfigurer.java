688568d2d69f40c5653249bbd6b660461c67deda
dewanto.rentenservice@googlemail.com <dewanto.rentenservice@googlemail.com@920737b8-6d35-43a0-81df-39543d9ce713>
2012-08-08 15:53:56 +0000

    add mocking with Mockito

--- java/components/extra-core/src/main/java/de/extra/client/core/annotation/PropertyPlaceholderPlugInConfigurer.java
+++ java/components/extra-core/src/main/java/de/extra/client/core/annotation/PropertyPlaceholderPlugInConfigurer.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package de.extra.client.core.annotation;
 
 import java.beans.PropertyDescriptor;
@@ -11,98 +29,135 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.factory.BeanCreationException;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
-import org.springframework.util.ReflectionUtils;
 
+/**
+ * This class extends the PropertyPlaceholderConfigurer so that we will able to
+ * configure our plugins. This code is taken and extended from: http://...
+ * 
+ * @author Leonid Potap
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class PropertyPlaceholderPlugInConfigurer extends
+		PropertyPlaceholderConfigurer {
 
-public class PropertyPlaceholderPlugInConfigurer extends PropertyPlaceholderConfigurer{
-	
+	private static final Logger logger = Logger
+			.getLogger(PropertyPlaceholderPlugInConfigurer.class);
 
-	    private static final Logger LOG = Logger.getLogger(PropertyPlaceholderPlugInConfigurer.class);
+	@Override
+	protected void processProperties(
+			ConfigurableListableBeanFactory beanFactory, Properties properties)
+			throws BeansException {
+		super.processProperties(beanFactory, properties);
 
-	    @Override
-	    protected void processProperties(ConfigurableListableBeanFactory beanFactory, Properties properties) throws BeansException {
-	        super.processProperties(beanFactory, properties);
+		for (String name : beanFactory.getBeanDefinitionNames()) {
+			Class<?> clazz = beanFactory.getType(name);
 
-	        for (String name : beanFactory.getBeanDefinitionNames()) {
-	            Class<?> clazz = beanFactory.getType(name);
+			if (logger.isDebugEnabled()) {
+				logger.debug("Configuring properties for bean=" + name + "["
+						+ clazz + "]");
+			}
 
-	            if(LOG.isDebugEnabled()){
-	            	LOG.debug("Configuring properties for bean="+name+"["+clazz+"]");
-	            }
-	            
-	            if(clazz != null && clazz.isAnnotationPresent(PlugInConfigutation.class)) {
-	            	PlugInConfigutation annotationConfigutation = clazz.getAnnotation(PlugInConfigutation.class);
-		            MutablePropertyValues mutablePropertyValues = beanFactory.getBeanDefinition(name).getPropertyValues();	
+			if (clazz != null
+					&& clazz.isAnnotationPresent(PlugInConfigutation.class)) {
+				PlugInConfigutation annotationConfigutation = clazz
+						.getAnnotation(PlugInConfigutation.class);
+				MutablePropertyValues mutablePropertyValues = beanFactory
+						.getBeanDefinition(name).getPropertyValues();
 
-	                for (PropertyDescriptor property : BeanUtils.getPropertyDescriptors(clazz)) {
-	                    Method setter = property.getWriteMethod();
-	                    PlugInValue valueAnnotation = null;
-	                    if(setter != null && setter.isAnnotationPresent(PlugInValue.class)) {
-	                    	valueAnnotation = setter.getAnnotation(PlugInValue.class);
-	                    }
-	                    if(valueAnnotation != null) {
-	                    	String key = extractKey (annotationConfigutation, valueAnnotation);
-	                        String value = resolvePlaceholder(key, properties, SYSTEM_PROPERTIES_MODE_FALLBACK);
-	                        if(StringUtils.isEmpty(value)) {
-	                            throw new BeanCreationException (name,"No such property=["+key+"] found in properties.");
-	                        }
-	                        if(LOG.isDebugEnabled()) {
-	                        	LOG.debug("setting property=["+clazz.getName()+"."+property.getName()+"] value=["+key+"="+value+"]");
-	                        }
-	                        mutablePropertyValues.addPropertyValue(property.getName(), value);
-	                    }
-	                }
+				for (PropertyDescriptor property : BeanUtils
+						.getPropertyDescriptors(clazz)) {
+					Method setter = property.getWriteMethod();
+					PlugInValue valueAnnotation = null;
+					if (setter != null
+							&& setter.isAnnotationPresent(PlugInValue.class)) {
+						valueAnnotation = setter
+								.getAnnotation(PlugInValue.class);
+					}
+					if (valueAnnotation != null) {
+						String key = extractKey(annotationConfigutation,
+								valueAnnotation);
+						String value = resolvePlaceholder(key, properties,
+								SYSTEM_PROPERTIES_MODE_FALLBACK);
+						if (StringUtils.isEmpty(value)) {
+							throw new BeanCreationException(name,
+									"No such property=[" + key
+											+ "] found in properties.");
+						}
+						if (logger.isDebugEnabled()) {
+							logger.debug("setting property=[" + clazz.getName()
+									+ "." + property.getName() + "] value=["
+									+ key + "=" + value + "]");
+						}
+						mutablePropertyValues.addPropertyValue(
+								property.getName(), value);
+					}
+				}
 
-	                for(Field field : clazz.getDeclaredFields()) {
-	                    if(LOG.isDebugEnabled()) {
-	                    	LOG.debug("examining field=["+clazz.getName()+"."+field.getName()+"]");
-	                    }
-	                    if(field.isAnnotationPresent(PlugInValue.class)) {
-	                    	PlugInValue valueAnnotation = field.getAnnotation(PlugInValue.class);
-	                        PropertyDescriptor property = BeanUtils.getPropertyDescriptor(clazz, field.getName());
+				for (Field field : clazz.getDeclaredFields()) {
+					if (logger.isDebugEnabled()) {
+						logger.debug("examining field=[" + clazz.getName()
+								+ "." + field.getName() + "]");
+					}
+					if (field.isAnnotationPresent(PlugInValue.class)) {
+						PlugInValue valueAnnotation = field
+								.getAnnotation(PlugInValue.class);
+						PropertyDescriptor property = BeanUtils
+								.getPropertyDescriptor(clazz, field.getName());
 
-	                        if(property == null || property.getWriteMethod() == null) {
-	                            throw new BeanCreationException(name,"setter for property=["+clazz.getName()+"."+field.getName()+"] not available.");
-	                        }
-	                        String key = extractKey (annotationConfigutation, valueAnnotation);
-	                        Object value = resolvePlaceholder(key, properties, SYSTEM_PROPERTIES_MODE_FALLBACK);
-	                        if(value == null) {
-	                            throw new BeanCreationException(name,"No such property=["+key+"] found in properties.");
-	                        }
-	                        if(LOG.isDebugEnabled()){
-	                        	LOG.debug("setting property=["+clazz.getName()+"."+field.getName()+"] value=["+key+"="+value+"]");
-	                        }
-	                        mutablePropertyValues.addPropertyValue(property.getName(), value);
-	                        if(LOG.isDebugEnabled()){
-	                        	LOG.debug("setting property=["+clazz.getName()+"."+field.getName()+"] value=["+key+"="+value+"]");
-	                        }
-	                        
+						if (property == null
+								|| property.getWriteMethod() == null) {
+							throw new BeanCreationException(name,
+									"setter for property=[" + clazz.getName()
+											+ "." + field.getName()
+											+ "] not available.");
+						}
+						String key = extractKey(annotationConfigutation,
+								valueAnnotation);
+						Object value = resolvePlaceholder(key, properties,
+								SYSTEM_PROPERTIES_MODE_FALLBACK);
+						if (value == null) {
+							throw new BeanCreationException(name,
+									"No such property=[" + key
+											+ "] found in properties.");
+						}
+						if (logger.isDebugEnabled()) {
+							logger.debug("setting property=[" + clazz.getName()
+									+ "." + field.getName() + "] value=[" + key
+									+ "=" + value + "]");
+						}
+						mutablePropertyValues.addPropertyValue(
+								property.getName(), value);
+						if (logger.isDebugEnabled()) {
+							logger.debug("setting property=[" + clazz.getName()
+									+ "." + field.getName() + "] value=[" + key
+									+ "=" + value + "]");
+						}
 
-	                        mutablePropertyValues.addPropertyValue(field.getName(), value);
-	                    }
-	                }
-	            }
-	        }
-	    }
-
-	    
-		/**
-		 * Findet Key Anhang Annotation
-		 * @param annotation
-		 * @return
-		 */
-		private String extractKey(PlugInConfigutation annotation, PlugInValue value) {
-			String initialKey = value.key();
-			String plugInBeanName = annotation.plugInBeanName();
-			String configPrefix = annotation.plugInType().getConfigPrefix();
-			StringBuilder key = new StringBuilder();
-			key.append(configPrefix).append(".").append(plugInBeanName).append(".").append(initialKey);
-			return key.toString();
+						mutablePropertyValues.addPropertyValue(field.getName(),
+								value);
+					}
+				}
+			}
 		}
+	}
 
-
+	/**
+	 * Findet Key Anhang Annotation.
+	 * 
+	 * @param annotation
+	 * @return
+	 */
+	private String extractKey(PlugInConfigutation annotation, PlugInValue value) {
+		String initialKey = value.key();
+		String plugInBeanName = annotation.plugInBeanName();
+		String configPrefix = annotation.plugInType().getConfigPrefix();
+		StringBuilder key = new StringBuilder();
+		key.append(configPrefix).append(".").append(plugInBeanName).append(".")
+				.append(initialKey);
+		return key.toString();
+	}
 
 }
---
+++ java/components/extra-core/src/test/java/de/extra/client/core/locator/PluginsLocatorManagerTest.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package de.extra.client.core.locator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import de.extra.client.core.plugin.IDataPlugin;
+
+/**
+ * Test for PluginsLocatorManager.
+ * 
+ * @author Lofi Dewanto
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class PluginsLocatorManagerTest {
+
+	private PlugInsLocatorManager pluginsLocatorManager;
+
+	@Mock
+	private Map<String, IDataPlugin> dataPlugInMap;
+
+	@Mock
+	IDataPlugin dataPlugin;
+
+	@Before
+	public void setUp() throws Exception {
+		pluginsLocatorManager = new PlugInsLocatorManager();
+	}
+
+	@Test
+	public void testGetConfiguratedDataPlugIn() {
+		when(dataPlugInMap.get(anyString())).thenReturn(dataPlugin);
+
+		pluginsLocatorManager.setDataPlugInMap(dataPlugInMap);
+		IDataPlugin resultDataPlugin = pluginsLocatorManager
+				.getConfiguratedDataPlugIn();
+
+		assertNotNull(resultDataPlugin);
+		assertEquals(dataPlugin, resultDataPlugin);
+
+		verify(dataPlugInMap, atLeastOnce()).get(anyString());
+	}
+
+	@Test
+	public void testGetConfiguratedOutputPlugin() {
+	}
+
+	@Test
+	public void testGetConfiguratedConfigPlugIn() {
+	}
+}
