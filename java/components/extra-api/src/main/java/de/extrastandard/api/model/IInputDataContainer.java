5de9fd7fda0ad6223744cc6b663cc60c47e74672
potap.rentenservice@gmail.com <potap.rentenservice@gmail.com@920737b8-6d35-43a0-81df-39543d9ce713>
2012-08-27 09:35:41 +0000

    Initial import.

---
+++ java/components/extra-api/src/main/java/de/extrastandard/api/model/IInputDataContainer.java
@@ -0,0 +1,20 @@
+package de.extrastandard.api.model;
+
+import java.io.InputStream;
+import java.util.List;
+
+
+public interface IInputDataContainer {
+
+	String getRequestId();
+
+	List<IInputDataPluginDescription> getPlugins();
+
+	String getDataRequestId();
+
+	/**
+	 * @return the inputData
+	 */
+	InputStream getInputData();
+
+}
