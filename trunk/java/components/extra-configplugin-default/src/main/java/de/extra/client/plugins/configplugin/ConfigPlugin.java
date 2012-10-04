4e631531d78d69a1962074ba540850e88860683e
potap.rentenservice@gmail.com <potap.rentenservice@gmail.com@920737b8-6d35-43a0-81df-39543d9ce713>
2012-10-04 11:55:40 +0000

    Große Refactoring. Namen nach der Architekturmodel angepasst

---
+++ trunk/java/components/extra-configplugin-default/src/main/java/de/extra/client/plugins/configplugin/ConfigPlugin.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package de.extra.client.plugins.configplugin;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.extra.client.plugins.configplugin.controller.ConfigPluginController;
+import de.extrastandard.api.model.content.IExtraProfileConfiguration;
+import de.extrastandard.api.plugin.IConfigPlugin;
+
+@Named("defaultConfigPlugin")
+public class ConfigPlugin implements IConfigPlugin {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ConfigPlugin.class);
+
+	@Inject
+	@Named("configController")
+	private ConfigPluginController configPluginController;
+
+	/**
+	 * Basismethode mit der die Verarbeitung der Konfigurationsdatei aufgerufen
+	 * wird.
+	 */
+	@Override
+	public IExtraProfileConfiguration getConfigFile() {
+		LOG.info("Aufruf der Verarbeitung");
+		return configPluginController.processConfigFile();
+	}
+}
