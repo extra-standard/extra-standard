419ffed5a40cc8e4c80a0222eddd42769eb3b831
potap.rentenservice@gmail.com <potap.rentenservice@gmail.com@920737b8-6d35-43a0-81df-39543d9ce713>
2012-10-04 07:33:17 +0000

    Scenarien SendFetch wird erweitert. Integration der Phase2

---
+++ trunk/java/components/extra-dataplugin-dbquery/src/main/java/de/extra/client/plugins/dataplugin/DBQueryDataPlugin.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package de.extra.client.plugins.dataplugin;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+
+import de.extra.client.core.model.DBQueryInputData;
+import de.extrastandard.api.model.content.IInputDataContainer;
+import de.extrastandard.api.model.execution.IExecutionPersistence;
+import de.extrastandard.api.model.execution.IInputData;
+import de.extrastandard.api.model.execution.PhaseQualifier;
+import de.extrastandard.api.plugin.IDataPlugin;
+
+/**
+ * Depending on the transit phase and the open Scenarion evaluates data from the
+ * database, and provides information for the further processing.
+ * 
+ * For example, for scenario SendFetch for Phase2, all input data is selected
+ * whose status "RESULT_EXPECTED", for the Phase3 all input data with the status
+ * "RESULT_PROCESSED"
+ * 
+ * @author Leonid Potap
+ * @version $Id$
+ */
+@Named("dbQueryDataPlugin")
+public class DBQueryDataPlugin implements IDataPlugin {
+
+	@Inject
+	@Named("executionPersistenceJPA")
+	IExecutionPersistence executionPersistence;
+
+	@Value("${core.execution.phase}")
+	private String executionPhase;
+
+	@Value("${core.execution.procedure}")
+	private String executionProcedure;
+
+	private static final Logger LOG = LoggerFactory.getLogger(DBQueryDataPlugin.class);
+
+	@Override
+	public Iterator<IInputDataContainer> getData() {
+		final List<IInputDataContainer> senderDataBeanList = new ArrayList<IInputDataContainer>();
+		final PhaseQualifier phaseQualifier = PhaseQualifier.resolveByName(executionPhase);
+		final List<IInputData> inputDataList = executionPersistence.findInputDataForExecution(executionProcedure,
+				phaseQualifier);
+		for (final IInputData inputData : inputDataList) {
+
+			final DBQueryInputData senderDataBean = new DBQueryInputData(String.valueOf(inputData.getId()),
+					inputData.getRequestId(), inputData.getResponseId());
+			senderDataBeanList.add(senderDataBean);
+		}
+
+		return senderDataBeanList.iterator();
+	}
+
+	/**
+	 * @param executionPhase
+	 *            the executionPhase to set
+	 */
+	public void setExecutionPhase(final String executionPhase) {
+		this.executionPhase = executionPhase;
+	}
+
+}
