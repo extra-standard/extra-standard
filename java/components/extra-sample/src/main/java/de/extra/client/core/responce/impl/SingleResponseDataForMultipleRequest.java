ec8d46fd78a8ab4b7332ffa1c7658a060e58e088
potap.rentenservice@gmail.com <potap.rentenservice@gmail.com@920737b8-6d35-43a0-81df-39543d9ce713>
2012-10-17 14:47:27 +0000

    first added for scenario with ine Acknoledge for Multiple Requests

---
+++ java/components/extra-sample/src/main/java/de/extra/client/core/responce/impl/SingleResponseDataForMultipleRequest.java
@@ -0,0 +1,82 @@
+package de.extra.client.core.responce.impl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import de.extrastandard.api.exception.ExceptionCode;
+import de.extrastandard.api.exception.ExtraConfigRuntimeException;
+import de.extrastandard.api.model.content.IResponseData;
+import de.extrastandard.api.model.content.ISingleResponseData;
+
+/**
+ * Wenn eine Nachricht mit mehreren Requests einen Response mit nur einer
+ * Acknoledge bekommt, ist diese Klasse für das Processing zu nutzen
+ * 
+ * @author Leonid Potap
+ * @version $Id$
+ */
+public class SingleResponseDataForMultipleRequest implements IResponseData {
+
+	ISingleResponseData singleResponseData;
+
+	/**
+	 * @param singleResponseData
+	 */
+	public SingleResponseDataForMultipleRequest(final ISingleResponseData singleResponseData) {
+		super();
+		this.singleResponseData = singleResponseData;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.extrastandard.api.model.content.IResponseData#getReponses()
+	 */
+	@Override
+	public Collection<ISingleResponseData> getResponses() {
+		final Collection<ISingleResponseData> responses = new ArrayList<ISingleResponseData>();
+		responses.add(singleResponseData);
+		return responses;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.extrastandard.api.model.content.IResponseData#getResponse(java.lang
+	 * .String)
+	 */
+	@Override
+	public ISingleResponseData getResponse(final String reguestId) {
+		return singleResponseData;
+	}
+
+	/**
+	 * Fügt einen neuen Datensatz zu den Responses
+	 * 
+	 * @param singleResponseData
+	 */
+	@Override
+	public void addSingleResponse(final ISingleResponseData singleResponseData) {
+		throw new ExtraConfigRuntimeException(ExceptionCode.EXTRA_ILLEGAL_ACCESS_EXCEPTION,
+				"SingleResponseDataForMultipleRequest.addSingleResponse method is not supported");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		final StringBuilder builder = new StringBuilder();
+		builder.append("SingleResponseDataForMultipleRequest [");
+		if (singleResponseData != null) {
+			builder.append("singleResponseData=");
+			builder.append(singleResponseData);
+		}
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
