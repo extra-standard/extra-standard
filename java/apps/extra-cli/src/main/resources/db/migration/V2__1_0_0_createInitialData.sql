321c20cf59b63f4044b77e7abddb7b2624383eae
dewanto.rentenservice@googlemail.com <dewanto.rentenservice@googlemail.com@920737b8-6d35-43a0-81df-39543d9ce713>
2012-10-29 14:16:11 +0000

    add database with Flyway

---
+++ java/apps/extra-cli/src/main/resources/db/migration/V2__1_0_0_createInitialData.sql
@@ -0,0 +1,10 @@
+--------------------------------------------------------
+-- Create Status DML.
+-- Oracle Database.
+--------------------------------------------------------
+
+Insert into STATUS  (ID, NAME) Values(1, 'INITIAL');
+Insert into STATUS (ID, NAME) Values (2, 'ENVELOPED');
+Insert into STATUS (ID, NAME) Values (3, 'TRANSMITTED');
+Insert into STATUS (ID, NAME) Values (8, 'FAIL');
+Insert into STATUS (ID, NAME) Values (7, 'DONE');
