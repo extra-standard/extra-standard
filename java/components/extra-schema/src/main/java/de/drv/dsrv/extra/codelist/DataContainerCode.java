5f8adb854be380b8c086a9cdc9cf75cafa1cff6c
potap.rentenservice@gmail.com <potap.rentenservice@gmail.com@920737b8-6d35-43a0-81df-39543d9ce713>
2012-12-28 11:06:38 +0000

    Extra Codes Konstanten

---
+++ java/components/extra-schema/src/main/java/de/drv/dsrv/extra/codelist/DataContainerCode.java
@@ -0,0 +1,17 @@
+package de.drv.dsrv.extra.codelist;
+
+/**
+ * A list of extensible codes denoting a data container type. Base values: -
+ * http://www.extra-standard.de/container/DATABASE -
+ * http://www.extra-standard.de/container/FILE
+ * 
+ * @author Leonid Potap
+ * @version $Id$
+ */
+public abstract class DataContainerCode {
+
+	public static final String FILE = "http://www.extra-standard.de/container/FILE";
+
+	public static final String DATABASE = "http://www.extra-standard.de/container/DATABASE";
+
+}
