846e3eb37e72b70fc672d24cf2c229dcd5b3e941
dewanto.rentenservice@googlemail.com <dewanto.rentenservice@googlemail.com@920737b8-6d35-43a0-81df-39543d9ce713>
2014-04-22 13:57:45 +0000

    add license Apache 2

--- java/apps/extra-spoc/extra-spoc-commons/src/main/java/de/extrastandard/spoc/commons/App.java
+++ java/apps/extra-spoc/extra-spoc-commons/src/main/java/de/extrastandard/spoc/commons/App.java
@@ -22,7 +22,7 @@ package de.extrastandard.spoc.commons;
  * Hello world!
  */
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
