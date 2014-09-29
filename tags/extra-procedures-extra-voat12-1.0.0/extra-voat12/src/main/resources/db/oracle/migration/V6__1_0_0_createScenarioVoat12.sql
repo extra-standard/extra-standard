f9c2097e20f98a4af9533f4fe542a3e2f150548b
potap.rentenservice@gmail.com <potap.rentenservice@gmail.com@920737b8-6d35-43a0-81df-39543d9ce713>
2014-09-29 07:47:57 +0000

    Release Fachverfahren Voat12. Erste Version 1.0.0

---
+++ tags/extra-procedures-extra-voat12-1.0.0/extra-voat12/src/main/resources/db/oracle/migration/V6__1_0_0_createScenarioVoat12.sql
@@ -0,0 +1,26 @@
+--------------------------------------------------------
+-- Initiale Datenbankbefuellung Sterbedatenabgleich
+-- Oracle Database.
+--------------------------------------------------------
+
+--Insert into MANDATOR (ID, NAME) Values (seq_mandator_id.nextval, 'DRV');
+
+Insert into PROCEDURE_TYPE (ID, NAME)
+ Values (seq_procedure_type_id.nextval, 'HollSendeBetrieb');
+   
+ 
+Insert into PROCEDURE_PHASE_CONFIGURATION
+   (ID, PHASE, PROCEDURE_TYPE_ID, NEXT_PHASE_CONFIGURATION_ID)
+ Values
+   (seq_procephase_config_id.nextval,
+    'PHASE1', 
+    (select id from procedure_type where name = 'HollSendeBetrieb'),
+    null);
+     
+Insert into PROCEDURE
+   (ID, NAME, SHORT_KEY, MANDATOR_ID, PROCEDURE_TYPE_ID)
+ Values
+   (seq_procedure_id.nextval, 'Voat12', 'Voat12', 
+   (select id from MANDATOR where name = 'DRV'),
+    (select id from PROCEDURE_TYPE where name = 'HollSendeBetrieb'));
+    
