e7e627f223f9406a7355953edcbbccb96b7d7c08
florianstratil <florianstratil@920737b8-6d35-43a0-81df-39543d9ce713>
2014-10-17 06:38:41 +0000

    Inital import of small eXTra-Client

---
+++ java/apps/extraClientLight/src/main/java/de/extra/extraClientLight/model/ResponseExtraBean.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package de.extra.extraClientLight.model;
+
+import de.extra.extraClientLight.model.enums.ReportLevelEnum;
+
+public class ResponseExtraBean {
+	
+	private int returnCode;
+	private String requestId;
+	private byte[] data;
+	private String responseId;
+	private ReportLevelEnum reportLevel;
+	private String reportCode;
+	private String reportText;
+	public int getReturnCode() {
+		return returnCode;
+	}
+	public void setReturnCode(int returnCode) {
+		this.returnCode = returnCode;
+	}
+	public String getRequestId() {
+		return requestId;
+	}
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+	public byte[] getData() {
+		return data;
+	}
+	public void setData(byte[] data) {
+		this.data = data;
+	}
+	public String getResponseId() {
+		return responseId;
+	}
+	public void setResponseId(String responseId) {
+		this.responseId = responseId;
+	}
+	public ReportLevelEnum getReportLevel() {
+		return reportLevel;
+	}
+	public void setReportLevel(ReportLevelEnum reportLevel) {
+		this.reportLevel = reportLevel;
+	}
+	public String getReportCode() {
+		return reportCode;
+	}
+	public void setReportCode(String reportCode) {
+		this.reportCode = reportCode;
+	}
+	public String getReportText() {
+		return reportText;
+	}
+	public void setReportText(String reportText) {
+		this.reportText = reportText;
+	}
+	
+	
+
+}
---
+++ java/apps/extraClientLight/src/main/java/de/extra/extraClientLight/util/ClientConstants.java
@@ -0,0 +1,28 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package de.extra.extraClientLight.util;
+
+public class ClientConstants {
+
+	public static final String EXTRA_VERSION = "1.3";
+	public static final String DETAILS_REQUEST_RESPONSE = "http://www.extra-standard.de/scenario/request-with-response";
+	public static final String DETAILS_ACKNOWLEDGEMENT = "http://www.extra-standard.de/scenario/request-with-acknowledgement";
+
+}
